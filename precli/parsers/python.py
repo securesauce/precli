2ab39f8199291734de9d06a28ddc917dff67d0fe
Eric Brown <ericwb@users.noreply.github.com>
2023-03-28 15:03:57 -0700

    Use abstract base class for parsers (#5)
    * New base_parser module to provide Parser abstract class
    * Common function moved into base class
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- precli/parsers/python.py
+++ precli/parsers/python.py
@@ -1,41 +1,11 @@
 # Copyright 2023 Secure Saurce LLC
-from importlib.metadata import entry_points
-
-import tree_sitter_languages
+from precli.parsers import base_parser
 
 
-def traverse_tree(tree):
-    cursor = tree.walk()
+class Python(base_parser.Parser):
 
-    reached_root = False
-    while reached_root is False:
-        yield cursor.node
-
-        if cursor.goto_first_child():
-            continue
-
-        if cursor.goto_next_sibling():
-            continue
-
-        retracing = True
-        while retracing:
-            if not cursor.goto_parent():
-                retracing = False
-                reached_root = True
-
-            if cursor.goto_next_sibling():
-                retracing = False
-
-
-class Python:
     def __init__(self):
-        self.language = tree_sitter_languages.get_language("python")
-        self.parser = tree_sitter_languages.get_parser("python")
-        self.rules = {}
-
-        discovered_rules = entry_points(group="precli.rules.python")
-        for rule in discovered_rules:
-            self.rules[rule.name] = rule.load()
+        super().__init__("python")
 
     def file_extension(self):
         return ".py"
@@ -43,7 +13,7 @@ class Python:
     def parse(self, data):
         tree = self.parser.parse(data)
 
-        for node in traverse_tree(tree):
+        for node in self.traverse_tree(tree):
             if node.type == "call":
                 print(node)
                 for rule in self.rules.values():
