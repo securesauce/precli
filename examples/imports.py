de0ffb6df8de6b22fd0784180e3143e9c2929f48
Eric Brown <eric.brown@securesauce.dev>
2023-03-29 22:45:55 -0700

    Parser handling of import and import from statements
    Parses and stores to dict the imports. Handles a variety
    of manually tested import statements.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

---
+++ examples/imports.py
@@ -0,0 +1,21 @@
+import os
+import yaml.load
+import sys, time, date
+import sys as s, time as t
+
+import hashlib as h
+import pickle.dumps as d
+
+from subprocess import Popen as pop
+
+from subprocess import Popen
+from subprocess import Popen, signal
+from pickle import loads as lp
+
+from subprocess import Popen as pop, signal as sig
+
+from ..foo import sys
+from . import sys
+from .. import sys
+from .. import subprocess
+from ..subprocess import Popen
--- precli/parsers/python.py
+++ precli/parsers/python.py
@@ -10,11 +10,69 @@ class Python(base_parser.Parser):
     def file_extension(self):
         return ".py"
 
+    def child_by_type(self, node, type):
+        # Return first child with type as specified
+        child = list(filter(lambda x: x.type == type, node.children))
+        return child[0] if child else None
+
+    def import_statement(self, nodes):
+        imports = dict()
+        for child in nodes:
+            if child.type == "dotted_name":
+                imports[child.text] = child.text
+            elif child.type == "aliased_import":
+                module = self.child_by_type(child, "dotted_name")
+                alias = self.child_by_type(child, "identifier")
+                imports[alias.text] = module.text
+        return imports
+
+    def import_from_statement(self, nodes):
+        imports = dict()
+
+        # Skip over the "from" node
+        next(nodes)
+
+        module = next(nodes)
+        if module.type == "dotted_name":
+            from_module = module.text
+        elif module.type == "relative_import":
+            # No known way to resolve the relative to absolute
+            from_module = b""
+
+        result = self.import_statement(nodes)
+        for key, value in result.items():
+            full_qual = [from_module, value]
+            imports[key] = b".".join(filter(None, full_qual))
+
+        return imports
+
     def parse(self, data):
         tree = self.parser.parse(data)
 
+        imports = dict()
+
         for node in self.traverse_tree(tree):
-            if node.type == "call":
-                print(node)
-                for rule in self.rules.values():
-                    rule()
+            match node.type:
+                case "import_statement":
+                    imps = self.import_statement(iter(node.children))
+                    imports.update(imps)
+
+                case "import_from_statement":
+                    imps = self.import_from_statement(iter(node.children))
+                    imports.update(imps)
+
+                case "call":
+                    #print(node)
+                    #print(node.text)
+                    #print(node.start_point)
+                    #print(node.end_point)
+                    #for rule in self.rules.values():
+                    #    rule()
+                    pass
+
+                case _:
+                    #print("Unknown node type.")
+                    pass
+
+        print("")
+        print(imports)
