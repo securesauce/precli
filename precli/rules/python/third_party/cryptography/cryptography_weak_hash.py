f359b3b4dd10f213aaa8d46a3a8bf0c91fe7c64e
Eric Brown <eric.brown@securesauce.dev>
2023-04-18 21:49:56 -0700

    Add cryptography weak hashes check
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

---
+++ precli/rules/python/third_party/cryptography/cryptography_weak_hash.py
@@ -0,0 +1,31 @@
+# Copyright 2023 Secure Saurce LLC
+from precli.core.level import Level
+from precli.core.result import Result
+from precli.core.rule import Rule
+
+
+class CryptographyWeakHash(Rule):
+    def __init__(self):
+        super().__init__(
+            id="PRE301",
+            name="reversible_one-way_hash",
+            full_descr=__doc__,
+            cwe=328,
+            message="Use of weak hash function {} does not meet security "
+            "expectations.",
+        )
+
+    def analyze(self, context: dict) -> Result:
+        if Rule.match_calls(
+            context,
+            [
+                "cryptography.hazmat.primitives.hashes.MD5",
+                "cryptography.hazmat.primitives.hashes.SHA1",
+            ],
+        ):
+            return Result(
+                rule_id=self.id,
+                context=context,
+                level=Level.ERROR,
+                message=self.message.format(context["func_call_qual"]),
+            )
