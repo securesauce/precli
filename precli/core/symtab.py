b5749da309d7d7ad71cde098c000eaef5a1b6f24
Eric Brown <eric.brown@securesauce.dev>
2023-05-03 21:42:57 -0700

    Redo symbol table to more polished solution
    * Use symbol table class
    * Use references to parent instead of lists of tables
    * Use a symbol class to represent name, type, value and possibly
      more in the future
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

---
+++ precli/core/symtab.py
@@ -0,0 +1,48 @@
+# Copyright 2023 Secure Saurce LLC
+from typing import Self
+
+
+class SymbolTable:
+    def __init__(self, parent=None):
+        self._symbols = {}
+        self._parent = parent
+
+    def parent(self) -> Self:
+        return self._parent
+
+    def put(self, name: str, type: str, value: str):
+        self._symbols[name] = Symbol(name, type, value)
+
+    def get(self, name: str):
+        if name in self._symbols:
+            return self._symbols[name]
+        elif self._parent is not None:
+            return self._parent.get(name)
+        else:
+            return None
+
+    def remove(self, name: str):
+        if name in self._symbols:
+            del self._symbols[name]
+
+    def __str__(self) -> str:
+        return str(self._symbols)
+
+
+class Symbol:
+    def __init__(self, name, type, value):
+        self._name = name
+        self._type = type
+        self._value = value
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @property
+    def value(self) -> str:
+        return self._value
