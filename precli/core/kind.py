9af8bfe05c212cb7e4dcafa2e801916d547d5e32
Eric Brown <eric.brown@securesauce.dev>
2023-06-14 11:52:06 -0700

    Add kind property to result
    Kind indicates the nature of the result. Typically this is "fail"
    or "pass", but could also be any one of the values in the Kind
    enum.
    
    See 3.27.9 kind property of:
    
    https://docs.oasis-open.org/sarif/sarif/v2.1.0/os/sarif-v2.1.0-os.html#_Toc34317647
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

---
+++ precli/core/kind.py
@@ -0,0 +1,33 @@
+# Copyright 2023 Secure Saurce LLC
+import enum
+
+
+class Kind(str, enum.Enum):
+    """
+    The nature of the result.
+
+    :var FAIL: A problem was found.
+    :vartype FAIL: str
+
+    :var REVIEW: Requires review to decide if it represents a problem.
+    :vartype REVIEW: str
+
+    :var NOT_APPLICABLE: Was not evaluated because it does not apply.
+    :vartype NOT_APPLICABLE: str
+
+    :var INFORMATIONAL: Info that does not indicate the presence of a problem.
+    :vartype INFORMATIONAL: str
+
+    :var OPEN: Insufficient information to decide whether a problem exists.
+    :vartype OPEN: str
+
+    :var PASS: No problem was found.
+    :vartype PASS: str
+    """
+
+    FAIL = "fail"
+    REVIEW = "review"
+    NOT_APPLICABLE = "notApplicable"
+    INFORMATIONAL = "informational"
+    OPEN = "open"
+    PASS = "pass"
