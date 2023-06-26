d5616109a046a725a39b8771dc24b3446ee74c65
Eric Brown <ericwb@users.noreply.github.com>
2023-06-26 13:32:28 -0700

    Hmac allows hashlib hashes as digest (#105)
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

---
+++ tests/unit/rules/python/stdlib/hmac/examples/hmac_new_digestmod_hashlib_sha3_224.py
@@ -0,0 +1,6 @@
+import hmac
+
+
+key = b"my-secret-key"
+message = b"Hello, world!"
+hmac.new(key, msg=message, digestmod=hashlib.sha3_224)
--- tests/unit/rules/python/stdlib/hmac/test_hmac_weak_hash.py
+++ tests/unit/rules/python/stdlib/hmac/test_hmac_weak_hash.py
@@ -42,6 +42,148 @@ class HmacWeakHashTests(test_case.TestCase):
         )
         self.assertEqual(0, len(results))
 
+    def test_hmac_digest_hashlib_blake2b(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_blake2b.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_blake2s(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_blake2s.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_md4(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_md4.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(7, result.location.start_line)
+        self.assertEqual(7, result.location.end_line)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(44, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_digest_hashlib_md5(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_md5.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(7, result.location.start_line)
+        self.assertEqual(7, result.location.end_line)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(44, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_digest_hashlib_ripemd160(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_ripemd160.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(7, result.location.start_line)
+        self.assertEqual(7, result.location.end_line)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(50, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_digest_hashlib_sha(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(7, result.location.start_line)
+        self.assertEqual(7, result.location.end_line)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(44, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_digest_hashlib_sha1(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha1.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(7, result.location.start_line)
+        self.assertEqual(7, result.location.end_line)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(45, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_digest_hashlib_sha224(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha224.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_sha256(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha256.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_sha384(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha384.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_sha3_224(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha384.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_sha3_256(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha384.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_sha3_384(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha384.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_sha3_512(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha384.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_sha512(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_sha512.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_shake_128(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_shake_128.py")
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_digest_hashlib_shake_256(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_digest_hashlib_shake_256.py")
+        )
+        self.assertEqual(0, len(results))
+
     def test_hmac_digest_md4(self):
         results = self.parser.parse(
             os.path.join(self.base_path, "hmac_digest_md4.py")
@@ -51,8 +193,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(11, result.location.end_column)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(38, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -65,8 +207,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(11, result.location.end_column)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(38, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -79,8 +221,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(11, result.location.end_column)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(44, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -93,8 +235,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(11, result.location.end_column)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(38, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -107,8 +249,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(11, result.location.end_column)
+        self.assertEqual(33, result.location.start_column)
+        self.assertEqual(39, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -184,6 +326,174 @@ class HmacWeakHashTests(test_case.TestCase):
         )
         self.assertEqual(0, len(results))
 
+    def test_hmac_new_digestmod_hashlib_blake2b(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_blake2b.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_blake2s(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_blake2s.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_md4(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_new_digestmod_hashlib_md4.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(6, result.location.start_line)
+        self.assertEqual(6, result.location.end_line)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(48, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_new_digestmod_hashlib_md5(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_new_digestmod_hashlib_md5.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(6, result.location.start_line)
+        self.assertEqual(6, result.location.end_line)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(48, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_new_digestmod_hashlib_ripemd160(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_ripemd160.py"
+            )
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(6, result.location.start_line)
+        self.assertEqual(6, result.location.end_line)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(54, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_new_digestmod_hashlib_sha(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_new_digestmod_hashlib_sha.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(6, result.location.start_line)
+        self.assertEqual(6, result.location.end_line)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(48, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_new_digestmod_hashlib_sha1(self):
+        results = self.parser.parse(
+            os.path.join(self.base_path, "hmac_new_digestmod_hashlib_sha1.py")
+        )
+        self.assertEqual(1, len(results))
+        result = results[0]
+        self.assertEqual("PRE0005", result.rule_id)
+        self.assertEqual(6, result.location.start_line)
+        self.assertEqual(6, result.location.end_line)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(49, result.location.end_column)
+        self.assertEqual(Level.ERROR, result.level)
+        self.assertEqual(-1.0, result.rank)
+
+    def test_hmac_new_digestmod_hashlib_sha224(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha224.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_sha256(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha256.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_sha384(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha384.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_sha3_224(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha3_224.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_sha3_256(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha3_256.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_sha3_384(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha3_384.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_sha3_512(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha3_512.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_sha512(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_sha512.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_shake_128(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_shake_128.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
+    def test_hmac_new_digestmod_hashlib_shake_256(self):
+        results = self.parser.parse(
+            os.path.join(
+                self.base_path, "hmac_new_digestmod_hashlib_shake_256.py"
+            )
+        )
+        self.assertEqual(0, len(results))
+
     def test_hmac_new_digestmod_md4(self):
         results = self.parser.parse(
             os.path.join(self.base_path, "hmac_new_digestmod_md4.py")
@@ -193,8 +503,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(8, result.location.end_column)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(42, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -207,8 +517,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(8, result.location.end_column)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(42, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -221,8 +531,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(8, result.location.end_column)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(48, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -235,8 +545,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(8, result.location.end_column)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(42, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
@@ -249,8 +559,8 @@ class HmacWeakHashTests(test_case.TestCase):
         self.assertEqual("PRE0005", result.rule_id)
         self.assertEqual(6, result.location.start_line)
         self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(8, result.location.end_column)
+        self.assertEqual(37, result.location.start_column)
+        self.assertEqual(43, result.location.end_column)
         self.assertEqual(Level.ERROR, result.level)
         self.assertEqual(-1.0, result.rank)
 
