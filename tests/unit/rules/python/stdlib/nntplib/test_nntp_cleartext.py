8d2eba04fa95c35fe53e3401d069181fbd046cd5
Eric Brown <eric.brown@securesauce.dev>
2023-11-13 19:52:13 -0800

    Auto generate nntplib tests
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/stdlib/nntplib/test_nntp_cleartext.py
+++ tests/unit/rules/python/stdlib/nntplib/test_nntp_cleartext.py
@@ -1,17 +1,17 @@
 # Copyright 2023 Secure Saurce LLC
 import os
 
+from parameterized import parameterized
+
 from precli.core.level import Level
 from precli.rules import Rule
 from tests.unit.rules.python import test_case
 
 
-RULE_ID = "PRE0011"
-
-
 class NntpCleartextTests(test_case.TestCase):
     def setUp(self):
         super().setUp()
+        self.rule_id = "PRE0011"
         self.base_path = os.path.join(
             "tests",
             "unit",
@@ -23,53 +23,24 @@ class NntpCleartextTests(test_case.TestCase):
         )
 
     def test_nntp_cleartext_rule_meta(self):
-        rule = Rule.get_by_id(RULE_ID)
-        self.assertEqual(RULE_ID, rule.id)
+        rule = Rule.get_by_id(self.rule_id)
+        self.assertEqual(self.rule_id, rule.id)
         self.assertEqual("cleartext_transmission", rule.name)
         self.assertEqual(
-            f"https://docs.securesauce.dev/rules/{RULE_ID}", rule.help_url
+            f"https://docs.securesauce.dev/rules/{self.rule_id}", rule.help_url
         )
         self.assertEqual(True, rule.default_config.enabled)
         self.assertEqual(Level.WARNING, rule.default_config.level)
         self.assertEqual(-1.0, rule.default_config.rank)
         self.assertEqual("319", rule.cwe.cwe_id)
 
-    def test_nntplib_nntp_context_mgr(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "nntplib_nntp_context_mgr.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(5, result.location.start_line)
-        self.assertEqual(5, result.location.end_line)
-        self.assertEqual(6, result.location.start_column)
-        self.assertEqual(11, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_nntplib_nntp_login(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "nntplib_nntp_login.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(5, result.location.start_line)
-        self.assertEqual(5, result.location.end_line)
-        self.assertEqual(2, result.location.start_column)
-        self.assertEqual(7, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_nntplib_nntp_ssl(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "nntplib_nntp_ssl.py")
-        )
-        self.assertEqual(0, len(results))
-
-    def test_nntplib_nntp_starttls(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "nntplib_nntp_starttls.py")
-        )
-        self.assertEqual(0, len(results))
+    @parameterized.expand(
+        [
+            "nntplib_nntp_context_mgr",
+            "nntplib_nntp_login",
+            "nntplib_nntp_ssl",
+            "nntplib_nntp_starttls",
+        ]
+    )
+    def test(self, filename):
+        self.check(filename)
