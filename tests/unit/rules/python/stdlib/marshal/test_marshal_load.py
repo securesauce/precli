a5dc7d95eeacef570d4c44233ff6c2a7d4995a51
Eric Brown <ericwb@users.noreply.github.com>
2023-11-13 19:48:23 -0800

    Auto generate marshal tests (#156)
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/stdlib/marshal/test_marshal_load.py
+++ tests/unit/rules/python/stdlib/marshal/test_marshal_load.py
@@ -1,17 +1,17 @@
 # Copyright 2023 Secure Saurce LLC
 import os
 
+from parameterized import parameterized
+
 from precli.core.level import Level
 from precli.rules import Rule
 from tests.unit.rules.python import test_case
 
 
-RULE_ID = "PRE0010"
-
-
 class MarshalLoadTests(test_case.TestCase):
     def setUp(self):
         super().setUp()
+        self.rule_id = "PRE0010"
         self.base_path = os.path.join(
             "tests",
             "unit",
@@ -23,41 +23,22 @@ class MarshalLoadTests(test_case.TestCase):
         )
 
     def test_marshal_load_rule_meta(self):
-        rule = Rule.get_by_id(RULE_ID)
-        self.assertEqual(RULE_ID, rule.id)
+        rule = Rule.get_by_id(self.rule_id)
+        self.assertEqual(self.rule_id, rule.id)
         self.assertEqual("deserialization_of_untrusted_data", rule.name)
         self.assertEqual(
-            f"https://docs.securesauce.dev/rules/{RULE_ID}", rule.help_url
+            f"https://docs.securesauce.dev/rules/{self.rule_id}", rule.help_url
         )
         self.assertEqual(True, rule.default_config.enabled)
         self.assertEqual(Level.WARNING, rule.default_config.level)
         self.assertEqual(-1.0, rule.default_config.rank)
         self.assertEqual("502", rule.cwe.cwe_id)
 
-    def test_marshal_load(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "marshal_load.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(10, result.location.start_line)
-        self.assertEqual(10, result.location.end_line)
-        self.assertEqual(18, result.location.start_column)
-        self.assertEqual(30, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_marshal_loads(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "marshal_loads.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(5, result.location.start_line)
-        self.assertEqual(5, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(13, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
+    @parameterized.expand(
+        [
+            "marshal_load",
+            "marshal_loads",
+        ]
+    )
+    def test(self, filename):
+        self.check(filename)
