49c215c7b7b3f309d3d9a732703dc7fa6b40723a
Eric Brown <eric.brown@securesauce.dev>
2023-11-13 19:59:51 -0800

    Auto generate pickle tests
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/stdlib/pickle/test_pickle_load.py
+++ tests/unit/rules/python/stdlib/pickle/test_pickle_load.py
@@ -1,17 +1,17 @@
 # Copyright 2023 Secure Saurce LLC
 import os
 
+from parameterized import parameterized
+
 from precli.core.level import Level
 from precli.rules import Rule
 from tests.unit.rules.python import test_case
 
 
-RULE_ID = "PRE0012"
-
-
 class PickleLoadTests(test_case.TestCase):
     def setUp(self):
         super().setUp()
+        self.rule_id = "PRE0012"
         self.base_path = os.path.join(
             "tests",
             "unit",
@@ -23,55 +23,23 @@ class PickleLoadTests(test_case.TestCase):
         )
 
     def test_pickle_load_rule_meta(self):
-        rule = Rule.get_by_id(RULE_ID)
-        self.assertEqual(RULE_ID, rule.id)
+        rule = Rule.get_by_id(self.rule_id)
+        self.assertEqual(self.rule_id, rule.id)
         self.assertEqual("deserialization_of_untrusted_data", rule.name)
         self.assertEqual(
-            f"https://docs.securesauce.dev/rules/{RULE_ID}", rule.help_url
+            f"https://docs.securesauce.dev/rules/{self.rule_id}", rule.help_url
         )
         self.assertEqual(True, rule.default_config.enabled)
         self.assertEqual(Level.WARNING, rule.default_config.level)
         self.assertEqual(-1.0, rule.default_config.rank)
         self.assertEqual("502", rule.cwe.cwe_id)
 
-    def test_pickle_load(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "pickle_load.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(6, result.location.start_line)
-        self.assertEqual(6, result.location.end_line)
-        self.assertEqual(10, result.location.start_column)
-        self.assertEqual(21, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_pickle_loads(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "pickle_loads.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(9, result.location.start_line)
-        self.assertEqual(9, result.location.end_line)
-        self.assertEqual(10, result.location.start_column)
-        self.assertEqual(22, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_pickle_loads(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "pickle_unpickler.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(6, result.location.start_line)
-        self.assertEqual(6, result.location.end_line)
-        self.assertEqual(10, result.location.start_column)
-        self.assertEqual(26, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
+    @parameterized.expand(
+        [
+            "pickle_load",
+            "pickle_loads",
+            "pickle_unpickler",
+        ]
+    )
+    def test(self, filename):
+        self.check(filename)
