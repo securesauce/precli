922a94ef5c510b0c2dd58526b7f70aa89d8b4fe1
Eric Brown <ericwb@users.noreply.github.com>
2023-11-14 15:35:20 -0800

    Create pyghmi tests (#169)
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

---
+++ tests/unit/rules/python/third_party/pyghmi/examples/command_console.py
@@ -0,0 +1,13 @@
+# level: ERROR
+# start_line: 9
+# end_line: 9
+# start_column: 6
+# end_column: 21
+from pyghmi.ipmi import command
+
+
+cmd = command.Console(
+    bmc="bmc",
+    userid="userid",
+    password="ZjE4ZjI0NTE4YmI2NGJjZDliOGY3ZmJiY2UyN2IzODQK",
+)
