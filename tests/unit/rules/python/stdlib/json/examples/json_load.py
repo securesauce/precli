784b47c23fe8daf47db93e6aeea1edae694a264e
Eric Brown <ericwb@users.noreply.github.com>
2023-11-13 19:36:30 -0800

    Auto generate json tests (#155)
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/stdlib/json/examples/json_load.py
+++ tests/unit/rules/python/stdlib/json/examples/json_load.py
@@ -1,3 +1,8 @@
+# level: WARNING
+# start_line: 11
+# end_line: 11
+# start_column: 0
+# end_column: 9
 import json
 from io import StringIO
 
--- tests/unit/rules/python/stdlib/json/test_json_load.py
+++ tests/unit/rules/python/stdlib/json/test_json_load.py
@@ -1,17 +1,17 @@
 # Copyright 2023 Secure Saurce LLC
 import os
 
+from parameterized import parameterized
+
 from precli.core.level import Level
 from precli.rules import Rule
 from tests.unit.rules.python import test_case
 
 
-RULE_ID = "PRE0008"
-
-
 class JsonLoadTests(test_case.TestCase):
     def setUp(self):
         super().setUp()
+        self.rule_id = "PRE0008"
         self.base_path = os.path.join(
             "tests",
             "unit",
@@ -23,55 +23,23 @@ class JsonLoadTests(test_case.TestCase):
         )
 
     def test_json_load_rule_meta(self):
-        rule = Rule.get_by_id(RULE_ID)
-        self.assertEqual(RULE_ID, rule.id)
+        rule = Rule.get_by_id(self.rule_id)
+        self.assertEqual(self.rule_id, rule.id)
         self.assertEqual("deserialization_of_untrusted_data", rule.name)
         self.assertEqual(
-            f"https://docs.securesauce.dev/rules/{RULE_ID}", rule.help_url
+            f"https://docs.securesauce.dev/rules/{self.rule_id}", rule.help_url
         )
         self.assertEqual(True, rule.default_config.enabled)
         self.assertEqual(Level.WARNING, rule.default_config.level)
         self.assertEqual(-1.0, rule.default_config.rank)
         self.assertEqual("502", rule.cwe.cwe_id)
 
-    def test_json_jsondecoder_decode(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "json_jsondecoder_decode.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(5, result.location.start_line)
-        self.assertEqual(5, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(14, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_json_load(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "json_load.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(6, result.location.start_line)
-        self.assertEqual(6, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(9, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_json_loads(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "json_loads.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(0, result.location.start_column)
-        self.assertEqual(10, result.location.end_column)
-        self.assertEqual(Level.WARNING, result.level)
-        self.assertEqual(-1.0, result.rank)
+    @parameterized.expand(
+        [
+            "json_jsondecoder_decode",
+            "json_load",
+            "json_loads",
+        ]
+    )
+    def test(self, filename):
+        self.check(filename)
