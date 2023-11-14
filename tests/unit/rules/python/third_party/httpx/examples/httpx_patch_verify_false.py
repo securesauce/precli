abc0d67c8ab1e6fac702efa6315928791f7b5799
Eric Brown <eric.brown@securesauce.dev>
2023-11-14 10:34:19 -0800

    Auto generate httpx tests
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/third_party/httpx/examples/httpx_patch_verify_false.py
+++ tests/unit/rules/python/third_party/httpx/examples/httpx_patch_verify_false.py
@@ -1,3 +1,8 @@
+# level: ERROR
+# start_line: 9
+# end_line: 9
+# start_column: 40
+# end_column: 45
 import httpx
 
 
--- tests/unit/rules/python/third_party/httpx/test_no_certificate_verify.py
+++ tests/unit/rules/python/third_party/httpx/test_no_certificate_verify.py
@@ -1,19 +1,19 @@
 # Copyright 2023 Secure Saurce LLC
 import os
 
+from parameterized import parameterized
+
 from precli.core.level import Level
 from precli.parsers import python
 from precli.rules import Rule
 from tests.unit.rules.python import test_case
 
 
-RULE_ID = "PRE0506"
-
-
 class NoCertificateVerifyTests(test_case.TestCase):
     def setUp(self):
         super().setUp()
-        self.parser = python.Python(enabled=[RULE_ID])
+        self.rule_id = "PRE0506"
+        self.parser = python.Python(enabled=[self.rule_id])
         self.base_path = os.path.join(
             "tests",
             "unit",
@@ -25,211 +25,35 @@ class NoCertificateVerifyTests(test_case.TestCase):
         )
 
     def test_no_certificate_verify_rule_meta(self):
-        rule = Rule.get_by_id(RULE_ID)
-        self.assertEqual(RULE_ID, rule.id)
+        rule = Rule.get_by_id(self.rule_id)
+        self.assertEqual(self.rule_id, rule.id)
         self.assertEqual("improper_certificate_validation", rule.name)
         self.assertEqual(
-            f"https://docs.securesauce.dev/rules/{RULE_ID}", rule.help_url
+            f"https://docs.securesauce.dev/rules/{self.rule_id}", rule.help_url
         )
         self.assertEqual(True, rule.default_config.enabled)
         self.assertEqual(Level.WARNING, rule.default_config.level)
         self.assertEqual(-1.0, rule.default_config.rank)
         self.assertEqual("295", rule.cwe.cwe_id)
 
-    def test_httpx_async_client_as_context_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(
-                self.base_path, "httpx_async_client_as_context_verify_false.py"
-            )
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(36, result.location.start_column)
-        self.assertEqual(41, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_async_client_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_async_client_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(34, result.location.start_column)
-        self.assertEqual(39, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_client_as_context_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(
-                self.base_path, "httpx_client_as_context_verify_false.py"
-            )
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(25, result.location.start_column)
-        self.assertEqual(30, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_client_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_client_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(29, result.location.start_column)
-        self.assertEqual(34, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_delete_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_delete_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(41, result.location.start_column)
-        self.assertEqual(46, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_get_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_get_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(38, result.location.start_column)
-        self.assertEqual(43, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_get_verify_true(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_get_verify_true.py")
-        )
-        self.assertEqual(0, len(results))
-
-    def test_httpx_get_verify_unset(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_get_verify_unset.py")
-        )
-        self.assertEqual(0, len(results))
-
-    def test_httpx_head_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_head_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(39, result.location.start_column)
-        self.assertEqual(44, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_options_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_options_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(42, result.location.start_column)
-        self.assertEqual(47, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_patch_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_patch_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(40, result.location.start_column)
-        self.assertEqual(45, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_post_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_post_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(39, result.location.start_column)
-        self.assertEqual(44, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_put_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_put_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(38, result.location.start_column)
-        self.assertEqual(43, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_request_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_request_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(48, result.location.start_column)
-        self.assertEqual(53, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
-
-    def test_httpx_stream_verify_false(self):
-        results = self.parser.parse(
-            os.path.join(self.base_path, "httpx_stream_verify_false.py")
-        )
-        self.assertEqual(1, len(results))
-        result = results[0]
-        self.assertEqual(RULE_ID, result.rule_id)
-        self.assertEqual(4, result.location.start_line)
-        self.assertEqual(4, result.location.end_line)
-        self.assertEqual(41, result.location.start_column)
-        self.assertEqual(46, result.location.end_column)
-        self.assertEqual(Level.ERROR, result.level)
-        self.assertEqual(-1.0, result.rank)
+    @parameterized.expand(
+        [
+            "httpx_async_client_as_context_verify_false",
+            "httpx_async_client_verify_false",
+            "httpx_client_as_context_verify_false",
+            "httpx_client_verify_false",
+            "httpx_delete_verify_false",
+            "httpx_get_verify_false",
+            "httpx_get_verify_true",
+            "httpx_get_verify_unset",
+            "httpx_head_verify_false",
+            "httpx_options_verify_false",
+            "httpx_patch_verify_false",
+            "httpx_post_verify_false",
+            "httpx_put_verify_false",
+            "httpx_request_verify_false",
+            "httpx_stream_verify_false",
+        ]
+    )
+    def test(self, filename):
+        self.check(filename)
