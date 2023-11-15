1b7e1bfa99581482eff9c887cecdcd2f2bea6639
Eric Brown <ericwb@users.noreply.github.com>
2023-11-14 17:58:17 -0800

    Add tests for pandas (#174)
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/third_party/pandas/test_pandas_read_pickle.py
+++ tests/unit/rules/python/third_party/pandas/test_pandas_read_pickle.py
@@ -9,10 +9,10 @@ from precli.rules import Rule
 from tests.unit.rules.python import test_case
 
 
-class PyghmiCleartextTests(test_case.TestCase):
+class PandasReadPickleTests(test_case.TestCase):
     def setUp(self):
         super().setUp()
-        self.rule_id = "PRE0517"
+        self.rule_id = "PRE0509"
         self.parser = python.Python(enabled=[self.rule_id])
         self.base_path = os.path.join(
             "tests",
@@ -20,28 +20,25 @@ class PyghmiCleartextTests(test_case.TestCase):
             "rules",
             "python",
             "third_party",
-            "pyghmi",
+            "pandas",
             "examples",
         )
 
-    def test_pyghmi_cleartext_rule_meta(self):
+    def test_pandas_read_pickle_rule_meta(self):
         rule = Rule.get_by_id(self.rule_id)
         self.assertEqual(self.rule_id, rule.id)
-        self.assertEqual("cleartext_transmission", rule.name)
+        self.assertEqual("deserialization_of_untrusted_data", rule.name)
         self.assertEqual(
             f"https://docs.securesauce.dev/rules/{self.rule_id}", rule.help_url
         )
         self.assertEqual(True, rule.default_config.enabled)
         self.assertEqual(Level.WARNING, rule.default_config.level)
         self.assertEqual(-1.0, rule.default_config.rank)
-        self.assertEqual("319", rule.cwe.cwe_id)
+        self.assertEqual("502", rule.cwe.cwe_id)
 
     @parameterized.expand(
         [
-            "command_command",
-            "command_command_no_password",
-            "command_console",
-            "command_console_no_password",
+            "pandas_read_pickle",
         ]
     )
     def test(self, filename):
