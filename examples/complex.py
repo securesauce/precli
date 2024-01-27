2ff23e14c64cba2ae3b3925183720a1f929305e2
Eric Brown <ericwb@users.noreply.github.com>
2024-01-26 19:31:59 -0800

    Remove the unused examples (#233)
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- examples/complex.py
+++
@@ -1,7 +0,0 @@
-import os
-import tempfile
-
-
-filename = tempfile.mktemp('', 'tmp', None)
-with os.open(filename, "w+", buffering=-1, encoding=None, errors=None, newline=None) as f:
-    f.write(b"Hello World!\n")
