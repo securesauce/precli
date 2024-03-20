9a72de6666c0ef51002063f3f60582ed88e96902
Eric Brown <ericwb@users.noreply.github.com>
2024-03-20 12:35:22 -0700

    Embed the copyright as part of the package (#374)
    Adds __copyright__ into the module.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- precli/__init__.py
+++ precli/__init__.py
@@ -1,8 +1,11 @@
+# Copyright 2024 Secure Saurce LLC
+from datetime import datetime
 from importlib import metadata
 
 
 __author__ = metadata.metadata("precli")["Author"]
 __summary__ = metadata.metadata("precli")["Summary"]
+__copyright__ = f"Copyright {datetime.now():%Y} Secure Saurce LLC"
 __download_url__ = metadata.metadata("precli")["Download-URL"]
 __url__ = metadata.metadata("precli")["Home-page"]
 __version__ = metadata.version("precli")
