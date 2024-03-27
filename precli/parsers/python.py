2dbd093613db9b608d6229ea244fbb68cc4713e0
Eric Brown <ericwb@users.noreply.github.com>
2024-03-26 21:29:43 -0700

    Fix for cases of a reassignment of a variable (#392)
    It is possible for a variable to be used multiple times and even with
    multiple types prior to its use in the function call. This didn't work
    because the left hand side of the assignment would be resolved into its
    complete fully qualified function name.
    
    However, it's also important to keep track of the identifier name in the
    symbol table in case it is reused. That way the value is properly
    overwritten.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- precli/parsers/python.py
+++ precli/parsers/python.py
@@ -115,7 +115,16 @@ class Python(Parser):
         ):
             left_hand = self.resolve(nodes[0], default=nodes[0])
             right_hand = self.resolve(nodes[2], default=nodes[2])
+
+            # This is in case a variable is reassigned
+            self.current_symtab.put(
+                nodes[0].text.decode(), tokens.IDENTIFIER, right_hand
+            )
+
+            # This is to help full resolution of an attribute/call.
+            # This results in two entries in the symtab for this assignment.
             self.current_symtab.put(left_hand, tokens.IDENTIFIER, right_hand)
+
             if nodes[2].type == tokens.CALL:
                 (call_args, call_kwargs) = self.get_func_args(
                     nodes[2].children[1]
---
+++ tests/unit/rules/python/stdlib/secrets/examples/secrets_token_bytes_size_var.py
@@ -0,0 +1,7 @@
+# level: NONE
+import secrets
+
+
+token_size = 16
+token_size = 32
+secrets.token_bytes(token_size)
