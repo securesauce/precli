8484212616991663139dabdae20112a736608816
Eric Brown <eric.brown@securesauce.dev>
2024-04-01 15:31:46 -0700

    Add suggested fixes for the hashlib weak hash rule
    The hashlib_weak_hash rule currently reports issues, but doesn't
    suggest fixes. This change will now introduce fix suggestions.
    Namely it will recommend using sha256.
    
    This commit also changed the location of the reported issue to be
    the actually function name or parameter so it is now more precise.
    This also requires updates to the tests.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/stdlib/hashlib/examples/hashlib_new_name_sha.py
+++ tests/unit/rules/python/stdlib/hashlib/examples/hashlib_new_name_sha.py
@@ -1,8 +1,8 @@
 # level: ERROR
 # start_line: 9
 # end_line: 9
-# start_column: 0
-# end_column: 11
+# start_column: 17
+# end_column: 22
 import hashlib
 
 
--- tests/unit/rules/python/stdlib/hashlib/examples/hashlib_sha1.py
+++ tests/unit/rules/python/stdlib/hashlib/examples/hashlib_sha1.py
@@ -1,7 +1,7 @@
 # level: ERROR
 # start_line: 9
 # end_line: 9
-# start_column: 0
+# start_column: 8
 # end_column: 12
 import hashlib
 
