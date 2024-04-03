7df237291c91a83c47a51592edec3c82e9b1b42d
Eric Brown <ericwb@users.noreply.github.com>
2024-04-03 13:13:31 -0700

    Resolve a call argument that is a call to its identifier (#412)
    This fixes the case when a call is analyzed that has a call as one of
    its arguments.
    
    foobar("foo", bar())
    
    Now the call in the argument list will resolve to an identifier node.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- precli/core/argument.py
+++ precli/core/argument.py
@@ -26,6 +26,8 @@ class Argument:
         if node is None:
             return None
         # TODO(ericwb): does this function fail with nested calls?
+        if node.type == tokens.CALL:
+            return Argument._get_func_ident(node.named_children[0])
         if node.type in [tokens.ATTRIBUTE, tokens.SELECTOR_EXPRESSION]:
             return Argument._get_func_ident(node.named_children[1])
         if node.type in [tokens.IDENTIFIER, tokens.FIELD_IDENTIFIER]:
