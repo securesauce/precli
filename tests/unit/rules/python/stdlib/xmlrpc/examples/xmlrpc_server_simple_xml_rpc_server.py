f1614a2796169b289e3e71c5a8b8a53c45ed0a2c
Eric Brown <eric.brown@securesauce.dev>
2024-05-01 21:45:21 -0700

    Update some tests so they are runnable
    Some of the test cases use code defined in fucntions but would
    not execute unless from some other module calling that function.
    
    This change updates each of these tests to add a main guard that
    calls the function.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/stdlib/xmlrpc/examples/xmlrpc_server_simple_xml_rpc_server.py
+++ tests/unit/rules/python/stdlib/xmlrpc/examples/xmlrpc_server_simple_xml_rpc_server.py
@@ -1,16 +1,16 @@
 # level: WARNING
-# start_line: 15
-# end_line: 15
+# start_line: 11
+# end_line: 11
 # start_column: 25
 # end_column: 39
-from xmlrpc.server import SimpleXMLRPCRequestHandler
 from xmlrpc.server import SimpleXMLRPCServer
 
 
-def run(
-    server_class: SimpleXMLRPCServer,
-    handler_class: SimpleXMLRPCRequestHandler,
-):
+def run(server_class: SimpleXMLRPCServer):
     server_address = ("0.0.0.0", 8000)
-    httpd = server_class(server_address, handler_class)
+    httpd = server_class(server_address, allow_none=True)
     httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run(SimpleXMLRPCServer)
