6ad1ab5bc0630eed98f10d57ab295f7936390314
Eric Brown <ericwb@users.noreply.github.com>
2024-05-01 21:51:27 -0700

    Update some tests so they are runnable (#451)
    Some of the test cases use code defined in fucntions but would not
    execute unless from some other module calling that function.
    
    This change updates each of these tests to add a main guard that calls
    the function.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- tests/unit/rules/python/stdlib/http/examples/http_server_threading_http_server.py
+++ tests/unit/rules/python/stdlib/http/examples/http_server_threading_http_server.py
@@ -1,16 +1,16 @@
 # level: WARNING
-# start_line: 15
-# end_line: 15
+# start_line: 11
+# end_line: 11
 # start_column: 25
 # end_column: 39
-from http.server import BaseHTTPRequestHandler
 from http.server import ThreadingHTTPServer
 
 
-def run(
-    server_class: ThreadingHTTPServer,
-    handler_class: BaseHTTPRequestHandler,
-):
+def run(server_class: ThreadingHTTPServer):
     server_address = ("", 8000)
-    httpd = server_class(server_address, handler_class)
+    httpd = server_class(server_address, allow_none=True)
     httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run(ThreadingHTTPServer)
--- tests/unit/rules/python/stdlib/xmlrpc/examples/xmlrpc_server_doc_xml_rpc_server.py
+++ tests/unit/rules/python/stdlib/xmlrpc/examples/xmlrpc_server_doc_xml_rpc_server.py
@@ -1,13 +1,16 @@
 # level: WARNING
-# start_line: 12
-# end_line: 12
+# start_line: 11
+# end_line: 11
 # start_column: 25
 # end_column: 39
-from xmlrpc.server import DocXMLRPCRequestHandler
 from xmlrpc.server import DocXMLRPCServer
 
 
-def run(server_class: DocXMLRPCServer, handler_class: DocXMLRPCRequestHandler):
+def run(server_class: DocXMLRPCServer):
     server_address = ("::", 8000)
-    httpd = server_class(server_address, handler_class)
+    httpd = server_class(server_address, allow_none=True)
     httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run(DocXMLRPCServer)
