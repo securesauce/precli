813c1b4a699a0b9b7ec8d2ae1a1adb3c778cce24
Eric Brown <eric.brown@securesauce.dev>
2025-03-04 14:46:11 -0800

    Add msgfmt step to tox
    This new msgfmt step of tox will compile the *.mo files into
    *.po binaries suitable to be loaded for specific locale.
    
    This change also add additional strings available to translate.
    
    Signed-off-by: Eric Brown <eric.brown@securesauce.dev>

--- precli/core/cwe.py
+++ precli/core/cwe.py
@@ -1,39 +1,40 @@
 # Copyright 2024 Secure Sauce LLC
 # SPDX-License-Identifier: BUSL-1.1
+from precli.i18n import _
 
 
 class Cwe:
     _cwe_names = {
-        79: (
+        79: _(
             "Improper Neutralization of Input During Web Page Generation "
             "('Cross-site Scripting')"
         ),
-        94: "Improper Control of Generation of Code ('Code Injection')",
-        208: "Observable Timing Discrepancy",
-        214: "Invocation of Process Using Visible Sensitive Information",
-        215: "Insertion of Sensitive Information Into Debugging Code",
-        250: "Execution with Unnecessary Privileges",
-        295: "Improper Certificate Validation",
-        319: "Cleartext Transmission of Sensitive Information",
-        326: "Inadequate Encryption Strength",
-        306: "Missing Authentication for Critical Function",
-        327: "Use of a Broken or Risky Cryptographic Algorithm",
-        328: "Use of Weak Hash",
-        330: "Use of Insufficiently Random Values",
-        338: (
+        94: _("Improper Control of Generation of Code ('Code Injection')"),
+        208: _("Observable Timing Discrepancy"),
+        214: _("Invocation of Process Using Visible Sensitive Information"),
+        215: _("Insertion of Sensitive Information Into Debugging Code"),
+        250: _("Execution with Unnecessary Privileges"),
+        295: _("Improper Certificate Validation"),
+        319: _("Cleartext Transmission of Sensitive Information"),
+        326: _("Inadequate Encryption Strength"),
+        306: _("Missing Authentication for Critical Function"),
+        327: _("Use of a Broken or Risky Cryptographic Algorithm"),
+        328: _("Use of Weak Hash"),
+        330: _("Use of Insufficiently Random Values"),
+        338: _(
             "Use of Cryptographically Weak Pseudo-Random Number Generator "
             "(PRNG)"
         ),
-        347: "Improper Verification of Cryptographic Signature",
-        377: "Insecure Temporary File",
-        502: "Deserialization of Untrusted Data",
-        598: "Use of GET Request Method With Sensitive Query Strings",
-        614: "Sensitive Cookie in HTTPS Session Without 'Secure' Attribute",
-        703: "Improper Check or Handling of Exceptional Conditions",
-        732: "Incorrect Permission Assignment for Critical Resource",
-        1088: "Synchronous Access of Remote Resource without Timeout",
-        1327: "Binding to an Unrestricted IP Address",
-        1333: "Inefficient Regular Expression Complexity",
+        347: _("Improper Verification of Cryptographic Signature"),
+        377: _("Insecure Temporary File"),
+        502: _("Deserialization of Untrusted Data"),
+        598: _("Use of GET Request Method With Sensitive Query Strings"),
+        614: _("Sensitive Cookie in HTTPS Session Without 'Secure' Attribute"),
+        703: _("Improper Check or Handling of Exceptional Conditions"),
+        732: _("Incorrect Permission Assignment for Critical Resource"),
+        1088: _("Synchronous Access of Remote Resource without Timeout"),
+        1327: _("Binding to an Unrestricted IP Address"),
+        1333: _("Inefficient Regular Expression Complexity"),
     }
 
     def __init__(self, id: int):
